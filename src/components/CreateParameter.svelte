4c28ecbf551795b8e1384f5d62abfdd56a3b28ee
Holden Greene <holdengreene26@gmail.com>
2020-07-25 13:21:36 -0600

    Changed parameter switcher

--- src/components/CreateParameter.svelte
+++ src/components/CreateParameter.svelte
@@ -21,8 +21,6 @@
   `;
 
   async function insertParameter(e) {
-    e.preventDefault();
-
     try {
       await client.mutate({
         mutation: INSERT_PARAMETER,
@@ -34,15 +32,15 @@
   }
 </script>
 
-<form action="#" method="post" on:submit={insertParameter}>
+<form action="#" method="post" on:submit|preventDefault={insertParameter}>
   <fieldset>
     <legend>Parameter Info</legend>
 
     <label for="p-name">Parameter Name</label>
-    <input type="text" id="p-name" bind:value={parameterName} />
+    <input type="text" name="parameterName" id="p-name" bind:value={parameterName} />
 
     <label for="p-value">Parameter Value</label>
-    <input type="text" id="p-value" bind:value={parameterValue} />
+    <input type="text" name="parameterValue" id="p-value" bind:value={parameterValue} />
   </fieldset>
 
   <button type="submit">Create</button>
--- src/components/MainChart.svelte
+++ src/components/MainChart.svelte
@@ -15,12 +15,12 @@
 
   $: if (parameters) {
     const selectedParameterValue = parameters
-      .filter(parameter => parameter.name === selectedParameter)
-      .map(parameter => parameter.value);
+      .filter((parameter) => parameter.name === selectedParameter)
+      .map((parameter) => parameter.value);
 
     const selectedParameterDate = parameters
-      .filter(parameter => parameter.name === selectedParameter)
-      .map(parameter => new Date(parameter.date_tested).toLocaleDateString());
+      .filter((parameter) => parameter.name === selectedParameter)
+      .map((parameter) => new Date(parameter.date_tested).toLocaleDateString());
 
     data = {
       labels: [...selectedParameterDate],
@@ -28,43 +28,66 @@
         {
           name: "Nitrate",
           type: "line",
-          values: [...selectedParameterValue]
-        }
-      ]
+          values: [...selectedParameterValue],
+        },
+      ],
     };
   }
 
   onMount(() => {
     chart = new Chart(chartRef, {
-      title: "Cool Chart",
+      title: "",
       data,
       type: "axis-mixed",
       height: 500,
-      color: ["#743ee2"]
+      color: ["#743ee2"],
     });
   });
 
   afterUpdate(() => chart && chart.update(data));
 
   onDestroy(() => (chart = null));
-
-  function changeParameter(parameterName) {
-    selectedParameter = parameterName;
-  }
 </script>
 
 <style>
+  h2 {
+    font-size: 34px;
+  }
+
   .chart {
     width: 100%;
   }
+
+  .chart-top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+
+  .parameter-selector {
+    font-size: 20px;
+    padding: 5px 30px;
+    border: none;
+    border-bottom: 2px solid #5d5d5d;
+  }
 </style>
 
-<div class="chart" bind:this={chartRef} />
 {#if availableParameters}
-  <p>Currently Selected: {selectedParameter}</p>
-  {#each availableParameters as parameter (parameter.id)}
-    <button on:click={() => changeParameter(parameter.name)}>
-      {parameter.name}
-    </button>
-  {/each}
+  <div class="chart-top">
+    <h2>{selectedParameter}</h2>
+
+    <select
+      class="parameter-selector"
+      name="parameter"
+      id="parameter"
+      bind:value={selectedParameter}>
+      {#each availableParameters as parameter (parameter.id)}
+        <option value={parameter.name}>{parameter.name}</option>
+      {/each}
+    </select>
+
+  </div>
 {/if}
+
+<div class="chart" bind:this={chartRef} />
